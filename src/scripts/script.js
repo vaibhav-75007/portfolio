d69d68194e1e4ea74db5295e88971de6f3fe3aaa
Vaibhav <52103891+Martian-dev@users.noreply.github.com>
2023-12-25 01:06:00 +0530

    finished contact form and made it send the message to a realtime firebase database

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,30 +1,68 @@
-function validateForm() {
-    var name = document.forms["contactForm"]["name"].value;
-    var email = document.forms["contactForm"]["email"].value;
-    var message = document.forms["contactForm"]["message"].value;
-    var error = "";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, set, push, } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-    if (name == "") {
-        error += "Please enter your name.\n";
-    }
+// Your web app's Firebase configuration
+// import firebaseConfig from '../dat.json' assert { type: 'json' };
+// const dat = await fetch('../../src/dat.json');
+// const firebaseConfig = await dat.json();
 
-    if (email == "") {
-        error += "Please enter your email address.\n";
-    } else if (!validateEmail(email)) {
-        error += "Please enter a valid email address.\n";
-    }
-
-    if (message == "") {
-        error += "Please enter a message.\n";
-    }
-
-    if (error != "") {
-        alert(error);
-        return false;
-    }
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 }
 
-function validateEmail(email) {
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const messagesRef = ref(database, "messages");
+
+/* function validateEmail(email) {
     var re = /\S+@\S+\.\S+/;
     return re.test(email);
-}
+} */
+
+
+const submitForm = (e) => {
+    e.preventDefault();
+
+    // getting values from the form
+    const name = getInputVal('name');
+    const email = getInputVal('email');
+    const message = getInputVal('message');
+
+    // saving the message
+    saveMessage(name, email, message)
+
+    document.querySelector('.alert').style.display = 'block';
+    setTimeout(() => {
+        document.querySelector('.alert').style.display = 'none';
+    }, 2000);
+
+    document.getElementById('contactForm').reset();
+};
+
+// function to get values instead of typing out that line every single time
+const getInputVal = (id) => {
+    return document.getElementById(id).value;
+};
+
+//function to save message to firebase
+const saveMessage = (name, email, message) => {
+    const newMessagesRef = push(messagesRef);
+    set(newMessagesRef, {
+        name,
+        email,
+        message
+    })
+};
+
+document.getElementById('contactForm').addEventListener('submit', submitForm);
